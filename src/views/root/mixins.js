5f95c1511d6d7f49071df5f74def632aa73d8205
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2017-11-11 15:18:29 +0900

    Add mixin

---
+++ src/views/root/mixins.js
@@ -0,0 +1,20 @@
+import _ from 'lodash'
+
+export let handleErrMixin = {
+  methods: {
+    handleErr (e) {
+      if (!e.response || e.response.status >= 500) {
+        this.$message.error(e.message)
+      } else {
+        _.forEach(e.response.data, v => {
+          this.$notify.error({
+            title: e.response.statusText,
+            message: v,
+            duration: 0
+          })
+        })
+      }
+      console.log(e)
+    }
+  }
+}
