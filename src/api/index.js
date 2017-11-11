688e4f3b1521a6ce961b3e486c0ef32827f66535
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2017-11-11 15:17:49 +0900

    Refactored api call

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,45 @@
-import { rejectStatusErr } from './lib'
+import axios from 'axios'
+import { getUserData, getAuthHeader } from '../lib/auth'
 
 export async function fetchToken (username, passwd) {
-  let resp = await fetch('/api/auth/token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
+  let res = await axios.post('/api/auth/token',
+    JSON.stringify({
       username: username,
       password: passwd
-    })
-  })
-  if (resp.status === 400) {
-    throw Error('ユーザー名またはパスワードが間違っています')
+    }), {
+      headers: {'Content-Type': 'application/json'}
+    }
+  )
+  return res.data
+}
+
+let client = axios.create({
+  baseURL: '/api/',
+  timeout: 1000,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
   }
-  return rejectStatusErr(resp)
+})
+
+export async function fetchMyItems () {
+  let user = getUserData()
+  let resp = await client.get('items', {
+    headers: getAuthHeader(),
+    params: {
+      owner: user.id
+    }
+  })
+  return resp.data
+}
+
+export async function fetchAllItems () {
+  let resp = await client.get('items', {
+    headers: getAuthHeader()
+  })
+  return resp.data
+}
+
+export async function deleteItem (id) {
+  return client.delete('items/' + id)
 }
