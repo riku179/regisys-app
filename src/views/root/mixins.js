4e9972da397c1f2623718bf78ff2574150606490
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2017-11-12 16:58:13 +0900

    Change to show error message when token is expired

--- src/views/root/mixins.js
+++ src/views/root/mixins.js
@@ -5,6 +5,8 @@ export let handleErrMixin = {
     handleErr (e) {
       if (!e.response || e.response.status >= 500) {
         this.$message.error(e.message)
+      } else if (e.response.status === 401) {
+        this.$message.error('トークンが期限切れです。リロードしてください')
       } else if (e.response.data) {
         _.forEach(e.response.data, v => {
           this.$notify.error({
