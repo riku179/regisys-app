c0124699c41b34c92311316a7c12522663fdc45a
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2017-11-23 17:45:13 +0900

    Dockernize

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+wait_db() {
+    echo -e "started to wait $API_HOST..."
+    until curl "$API_HOST:$API_PORT" > /dev/null 2>&1; do
+        >&2 echo -e "$API_HOST is unavailable - waiting..."I
+        sleep 3
+    done
+
+    >&2 echo "$API_HOST is up!"
+}
+
+wait_db
+
+nginx -g daemon off;
